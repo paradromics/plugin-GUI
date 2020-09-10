f7b74ed8c2e9bd1970097c1d4739945aef6b3676
Tyler Stern <tstern@paradromics.com>
2020-09-10 11:53:01 -0500

    Plugin template files / NIDAQ files
    combined the processor template files and the NIDAQ datathread files to get started on a datathread

---
+++ Plugins/NIOBReader/Source/ReadNIOB.cpp
@@ -0,0 +1,217 @@
+#include "ReadNIOB.h" // DOES NOT EXIST
+#include "NIOBEditor.h" // DOES NOT EXIST
+#include <stdexcept>
+
+DataThread* NIOBThread::createDataThread(SourceNode *sn)
+{
+    return new NIOBThread(sn);
+}
+
+GenericEditor* NIOBThread::createEditor(SourceNode* sn)
+{
+    return new NIOBEditor(sn, this, true);
+}
+
+NIOBThread::NIOBThread(SourceNode* sn) : DataThread(sn), inputAvailable(false)
+{
+
+//    dm = new NIOBmxDeviceManager();
+//
+//    dm->scanForDevices();
+//
+//    if (dm->getNumAvailableDevices() == 0)
+//    {
+//        //Okay for now as plugin-GUI handles source init runtime errors.
+//        throw std::runtime_error("No NIOB devices detected!");
+//    }
+
+    inputAvailable = true;
+    openConnection();
+
+}
+
+
+this is a test to see if it break s on compile
+
+
+NIOBThread::~NIOBThread()
+{
+}
+
+int NIOBThread::openConnection()
+{
+
+//    mNIOB = new NIOBmx(STR2CHR(dm->getDeviceFromIndex(0)));
+//
+//    sourceBuffers.add(new DataBuffer(getNumAnalogInputs(), 10000));
+//
+//    mNIOB->aiBuffer = sourceBuffers.getLast();
+//
+//    sampleRateIndex = mNIOB->sampleRates.size() - 1;
+//    setSampleRate(sampleRateIndex);
+//
+//    voltageRangeIndex = mNIOB->aiVRanges.size() - 1;
+//    setVoltageRange(voltageRangeIndex);
+
+    return 0;
+
+}
+
+//int NIOBThread::getNumAvailableDevices()
+//{
+//    return dm->getNumAvailableDevices();
+//}67
+
+
+//void NIOBThread::toggleSourceType(int id)
+//{
+//    NIOB->toggleSourceType(id);
+//}
+//
+//SOURCE_TYPE NIOBThread::getSourceTypeForInput(int index)
+//{
+//    return NIOB->st[index];
+//}
+
+void NIOBThread::closeConnection()
+{
+}
+
+//int NIOBThread::getNumAnalogInputs() const
+//{
+//    return mNIOB->ai.size();
+//}
+//
+//int NIOBThread::getNumDigitalInputs() const
+//{
+//    return mNIOB->di.size();
+//}
+//
+//void NIOBThread::toggleAIChannel(int index)
+//{
+//    mNIOB->aiChannelEnabled.set(index, !mNIOB->aiChannelEnabled[index]);
+//}
+//
+//void NIOBThread::toggleDIChannel(int index)
+//{
+//    mNIOB->diChannelEnabled.set(index, !mNIOB->diChannelEnabled[index]);
+//}
+//
+//void NIOBThread::setVoltageRange(int rangeIndex)
+//{
+//    voltageRangeIndex = rangeIndex;
+//    mNIOB->voltageRange = mNIOB->aiVRanges[rangeIndex];
+//}
+
+//void NIOBThread::setSampleRate(int rateIndex)
+//{
+//    sampleRateIndex = rateIndex;
+//    mNIOB->samplerate = mNIOB->sampleRates[rateIndex];
+//}
+//
+//int NIOBThread::getVoltageRangeIndex()
+//{
+//    return voltageRangeIndex;
+//}
+//
+//int NIOBThread::getSampleRateIndex()
+//{
+//    return sampleRateIndex;
+//}
+//
+//Array<String> NIOBThread::getVoltageRanges()
+//{
+//    Array<String> voltageRanges;
+//    for (auto range : mNIOB->aiVRanges)
+//    {
+//        voltageRanges.add(String(range.vmin) + "-" + String(range.vmax) + " V");
+//    }
+//    return voltageRanges;
+//}
+//
+//Array<String> NIOBThread::getSampleRates()
+//{
+//    Array<String> sampleRates;
+//    for (auto rate : mNIOB->sampleRates)
+//    {
+//        sampleRates.add(String(rate) + " S/s");
+//    }
+//    return sampleRates;
+//}
+
+//bool NIOBThread::foundInputSource()
+//{
+//    return inputAvailable;
+//}
+
+/** Initializes data transfer.*/
+bool NIOBThread::startAcquisition()
+{
+    NIOB->startThread();
+    this->startThread();
+    return true;
+}
+
+/** Stops data transfer.*/
+bool NIOBThread::stopAcquisition()
+{
+
+    if (isThreadRunning())
+    {
+        signalThreadShouldExit();
+    }
+    if (NIOB->isThreadRunning())
+    {
+        NIOB->signalThreadShouldExit();
+    }
+    return true;
+}
+//
+//void NIOBThread::setDefaultChannelNames()
+//{
+//
+//    for (int i = 0; i < getNumAnalogInputs(); i++)
+//    {
+//        ChannelCustomInfo info;
+//        info.name = "AI" + String(i + 1);
+//        info.gain = mNIOB->voltageRange.vmax / float(0x7fff);
+//        channelInfo.set(i, info);
+//    }
+//
+//}
+
+//bool NIOBThread::usesCustomNames() const
+//{
+//    return true;
+//}
+
+/** Returns the number of virtual subprocessors this source can generate */
+unsigned int NIOBThread::getNumSubProcessors() const
+{
+    return 1;
+}
+
+
+
+///** Returns the number of TTL channels that each subprocessor generates*/
+//int NIOBThread::getNumTTLOutputs(int subProcessorIdx) const
+//{
+//    return getNumDigitalInputs();
+//}
+
+/** Returns the sample rate of the data source.*/
+float NIOBThread::getSampleRate(int subProcessorIdx) const
+{
+    return 42;
+}
+
+float NIOBThread::getBitVolts(const DataChannel* chan) const
+{
+    return 4242;
+}
+
+
+bool NIOBThread::updateBuffer()
+{
+    return true;
+}
---
+++ Plugins/NIOBReader/Source/ReadNIOB.h
@@ -0,0 +1,264 @@
+//This prevents include loops. We recommend changing the macro to a name suitable for your plugin
+#ifndef ReadNIOB_H_DEFINED
+#define ReadNIOB_H_DEFINED
+
+#include <DataThreadHeaders.h>
+#include <stdio.h>
+#include <string.h>
+
+//namespace must be an unique name for your plugin
+namespace ReadNIOBSpace
+{
+	class ReadNIOB : public GenericProcessor
+	{
+	public:
+		/** The class constructor, used to initialize any members. */
+		ReadNIOB();
+
+		/** The class destructor, used to deallocate memory */
+		~ReadNIOB();
+        
+
+		/** Indicates if the processor has a custom editor. Defaults to false */
+		//bool hasEditor() const { return true; }
+
+		/** If the processor has a custom editor, this method must be defined to instantiate it. */
+		//AudioProcessorEditor* createEditor() override;
+
+		/** Optional method that informs the GUI if the processor is ready to function. If false acquisition cannot start. Defaults to true */
+		//bool isReady();
+
+		/** Defines the functionality of the processor.
+
+		The process method is called every time a new data buffer is available.
+
+		Processors can either use this method to add new data, manipulate existing
+		data, or send data to an external target (such as a display or other hardware).
+
+		Continuous signals arrive in the "buffer" variable, event data (such as TTLs
+		and spikes) is contained in the "events" variable.
+		*/
+		void process(AudioSampleBuffer& buffer) override;
+
+		/** Handles events received by the processor
+
+		Called automatically for each received event whenever checkForEvents() is called from process()		
+		*/
+		//void handleEvent(const EventChannel* eventInfo, const MidiMessage& event, int samplePosition) override;
+
+		/** Handles spikes received by the processor
+
+		Called automatically for each received event whenever checkForEvents(true) is called from process()
+		*/
+		//void handleSpike(const SpikeChannel* spikeInfo, const MidiMessage& event, int samplePosition) override;
+
+		/** The method that standard controls on the editor will call.
+		It is recommended that any variables used by the "process" function
+		are modified only through this method while data acquisition is active. */
+		//void setParameter(int parameterIndex, float newValue) override;
+
+		/** Saving custom settings to XML. */
+		//void saveCustomParametersToXml(XmlElement* parentElement) override;
+
+		/** Load custom settings from XML*/
+		//void loadCustomParametersFromXml() override;
+
+		/** Optional method called every time the signal chain is refreshed or changed in any way.
+
+		Allows the processor to handle variations in the channel configuration or any other parameter
+		passed down the signal chain. The processor can also modify here the settings structure, which contains
+		information regarding the input and output channels as well as other signal related parameters. Said
+		structure shouldn't be manipulated outside of this method.
+
+		*/
+		//void updateSettings() override;
+
+	};
+class NIOBThread : public DataThread, public Timer
+{
+
+public:
+
+    NIOBThread(SourceNode* sn);
+    ~NIOBThread();
+
+    bool updateBuffer();
+
+    void updateChannels();
+
+    /** Returns true if the data source is connected, false otherwise.*/
+    bool foundInputSource();
+
+    /** Returns version and serial number info for hardware and API as a string.*/
+    String getInfoString();
+
+    /** Returns version and serial number info for hardware and API as XML.*/
+    XmlElement getInfoXml();
+
+    void openConnection();
+
+    /** Initializes data transfer.*/
+    bool startAcquisition() override;
+
+    /** Stops data transfer.*/
+    bool stopAcquisition() override;
+
+    // DataThread Methods
+
+//    /** Returns the number of virtual subprocessors this source can generate */
+//    unsigned int getNumSubProcessors() const override;
+//
+//    /** Returns the number of continuous headstage channels the data source can provide.*/
+//    int getNumDataOutputs(DataChannel::DataChannelTypes type, int subProcessorIdx) const override;
+//
+//    /** Returns the number of TTL channels that each subprocessor generates*/
+//    int getNumTTLOutputs(int subProcessorIdx) const override;
+//
+//    /** Returns the sample rate of the data source.*/
+//    float getSampleRate(int subProcessorIdx) const override;
+//
+//    /** Returns the volts per bit of the data source.*/
+//    float getBitVolts(const DataChannel* chan) const override;
+//
+//    /** Used to set default channel names.*/
+//    void setDefaultChannelNames() override;
+//
+//    /** Used to override default channel names.*/
+//    bool usesCustomNames() const override;
+//
+//    /** Selects which electrode is connected to each channel. */
+//    void selectElectrodes(unsigned char slot, signed char port, Array<int> channelStatus);
+//
+//    /** Selects which reference is used for each channel. */
+//    void setAllReferences(unsigned char slot, signed char port, int refId);
+//
+//    /** Sets the gain for each channel. */
+//    void setAllGains(unsigned char slot, signed char port, unsigned char apGain, unsigned char lfpGain);
+//
+//    /** Sets the filter for all channels. */
+//    void setFilter(unsigned char slot, signed char port, bool filterState);
+//
+//    /** Toggles between internal and external triggering. */
+//    void setTriggerMode(bool trigger);
+//
+//    /** Toggles between saving to NPX file. */
+//    void setRecordMode(bool record);
+//
+//    /** Select directory for saving NPX files. */
+//    void setDirectoryForSlot(int slotIndex, File directory);
+//
+//    /** Select directory for saving NPX files. */
+//    File getDirectoryForSlot(int slotIndex);
+//
+//    /** Toggles between auto-restart setting. */
+//    void setAutoRestart(bool restart);
+//
+//    /** Starts data acquisition after a certain time.*/
+//    void timerCallback();
+
+    /** Starts recording.*/
+    void startRecording();
+
+    /** Stops recording.*/
+    void stopRecording();
+
+    CriticalSection* getMutex()
+    {
+        return &displayMutex;
+    }
+
+    static DataThread* createDataThread(SourceNode* sn);
+
+    GenericEditor* createEditor(SourceNode* sn);
+
+    int getNumBasestations();
+
+    void setMasterSync(int slotIndex);
+    void setSyncOutput(int slotIndex);
+
+    Array<int> getSyncFrequencies();
+    void setSyncFrequency(int slotIndex, int freqIndex);
+
+    ProbeStatus getProbeStatus(unsigned char slot, signed char port);
+    void setSelectedProbe(unsigned char slot, signed char probe);
+    bool isSelectedProbe(unsigned char slot, signed char probe);
+
+    bool checkSlotAndPortCombo(int slotIndex, int portIndex);
+    unsigned char getSlotForIndex(int slotIndex, int portIndex);
+    signed char getPortForIndex(int slotIndex, int portIndex);
+
+    bool runBist(unsigned char slot, signed char port, int bistIndex);
+
+    float getFillPercentage(unsigned char slot);
+
+    ScopedPointer<ProgressBar> progressBar;
+    double initializationProgress;
+
+    /* Helper for loading probes in the background */
+    struct probeSettings {
+        unsigned char slot;
+        signed char port;
+        Array<int> channelStatus;
+        int apGainIndex;
+        int lfpGainIndex;
+        int refChannelIndex;
+        bool disableHighPass;
+    } p_settings;
+    Array<probeSettings> probeSettingsUpdateQueue;
+
+    void updateProbeSettingsQueue();
+    void applyProbeSettingsQueue();
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(NeuropixThread);
+
+private:
+    bool baseStationAvailable;
+    bool internalTrigger;
+    bool autoRestart;
+
+    bool isRecording;
+
+    long int counter;
+    int recordingNumber;
+
+    CriticalSection displayMutex;
+
+    void closeConnection();
+
+    Array<int> channelMap;
+    Array<bool> outputOn;
+    Array<int> refs;
+
+    uint64_t probeId;
+
+    int maxCounter;
+    int numRefs;
+    int totalChans;
+    int totalProbes;
+
+    uint32_t last_niob_timestamp;
+
+    Array<float> fillPercentage;
+
+    OwnedArray<Basestation> basestations;
+
+
+    unsigned char selectedSlot;
+    signed char selectedPort;
+
+    
+
+    //std::vector<unsigned char> connected_basestations;
+    //std::vector<std::vector<int>> connected_probes;
+    
+    np::bistElectrodeStats stats[960];
+
+    RecordingTimer recordingTimer;
+
+};
+
+
+
+}
+
+#endif
